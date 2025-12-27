f90bd7cb9e15fc44834c311c251fa18231f16116
Maple4whom <jyandoc123@gmail.com>
2025-12-26 21:24:25 -0800

    Minor Code Update
    -Increased line width on plots

--- ILS_Lateral_Beam_Guidance_System_JoshuaYandoc.m
+++ ILS_Lateral_Beam_Guidance_System_JoshuaYandoc.m
@@ -108,45 +108,45 @@ Title.FontSize = 18;
 Title.FontWeight = 'bold';
 
 ax11 = nexttile(1);
-plot(tout, xout(1,:))
+plot(tout, xout(1,:), 'linewidth', 2)
 title('Simulation of current, i')
 xlabel('time[s]')
 ylabel('Current i [A]')
 ylim(ax11, [-0.5 0.25])
 
 ax12 = nexttile(2);
-plot(tout, xout(2,:))
+plot(tout, xout(2,:), 'linewidth', 2)
 title('Simulation of aileron deflection angle, \delta_a')
 xlabel('time[s]')
 ylabel('deflection angle \delta_a [deg]')
 
 ax13 = nexttile(3);
-plot(tout, xout(3,:))
+plot(tout, xout(3,:), 'linewidth', 2)
 title('Simulation of aileron deflection angle rate, d\delta_a/dt')
 xlabel('time[s]')
 ylabel('def. angle rate d\delta_a/dt [deg/s]')
 ylim(ax13, [-50 25])
 
 ax21 = nexttile(4);
-plot(tout, xout(4,:))
+plot(tout, xout(4,:), 'linewidth', 2)
 title('Simulation of bank angle, \phi')
 xlabel('time[s]')
 ylabel('bank angle \phi [deg]')
 
 ax22 = nexttile(5);
-plot(tout, xout(5,:))
+plot(tout, xout(5,:), 'linewidth', 2)
 title('Simulation of roll rate, d\phi/dt')
 xlabel('time[s]')
 ylabel('roll rate d\phi/dt [deg/s]')
 
 ax23 = nexttile(6);
-plot(tout, xout(6,:))
+plot(tout, xout(6,:), 'linewidth', 2)
 title('Simulation of heading angle, \psi')
 xlabel('time[s]')
 ylabel('heading angle \psi [deg]')
 
 ax32 = nexttile(8);
-plot(tout, xout(7,:))
+plot(tout, xout(7,:), 'linewidth', 2)
 title('Simulation of Lateral Displacement, Y_R')
 xlabel('time[s]')
 ylabel('lateral displacement, Y_R [m]')
@@ -171,25 +171,25 @@ tiledlayout(2,2)
 title(tiledlayout(2,2), 'State outputs from SIMULINK Model - Joshua Yandoc', 'Color', [1 0 0])
 
 nexttile
-plot(block_time, block_phi)
+plot(block_time, block_phi, 'linewidth', 2)
 title('SIMULINK Simulation of bank angle, \phi')
 xlabel('time[s]')
 ylabel('bank angle \phi [deg]')
 
 nexttile
-plot(block_time, block_p)
+plot(block_time, block_p, 'linewidth', 2)
 title('SIMULINK Simulation of roll rate, d\phi/dt')
 xlabel('time[s]')
 ylabel('roll rate d\phi/dt [deg/s]')
 
 nexttile
-plot(block_time, block_psi)
+plot(block_time, block_psi, 'linewidth', 2)
 title('SIMULINK Simulation of heading angle, \psi')
 xlabel('time[s]')
 ylabel('heading angle \psi [deg]')
 
 nexttile()
-plot(block_time, block_Y_R)
+plot(block_time, block_Y_R, 'linewidth', 2)
 title('SIMULINK Simulation of Lateral Displacement, Y_R')
 xlabel('time[s]')
 ylabel('lateral displacement, Y_R [m]')
